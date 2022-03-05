4570b79e929aaed2bb272325a0eb89941732d303
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-03-04 19:28:36 -0800

    Add new package: eslint-config-typescript

---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,107 @@
+const allExtensions = ['.js', '.jsx', '.mjs', '.json', '.ts', '.tsx'];
+
+module.exports = {
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      plugins: ['@typescript-eslint'],
+      parser: ['@typescript-eslint/parser'],
+      parserOptions: {
+        project: 'tsconfig.json',
+        createDefaultProgram: true,
+        tsconfigRootDir: './',
+      },
+      extends: [
+        // Disables rules that are already checked by the TypeScript compiler.
+        // Enables rules that promote using more modern constructs TypeScript allows for.
+        // Turn on recommended TS rules (opinionated)
+        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended.ts
+        'plugin:@typescript-eslint/recommended',
+
+        // Make eslint-plugin-import work with TypeScript
+        // https://github.com/import-js/eslint-plugin-import/blob/main/config/typescript.js
+        'plugin:import/typescript',
+      ],
+      settings: {
+        // File extensions that will be parsed as modules and inspected for exports
+        'import/extensions': allExtensions,
+        'import/resolver': {
+          node: {
+            extensions: allExtensions,
+          },
+          // Turns on the typescript resolver
+          // https://github.com/alexgorbatchev/eslint-import-resolver-typescript#readme
+          typescript: {
+            project: './tsconfig.json',
+            alwaysTryTypes: true,
+          },
+        },
+      },
+      rules: {
+        // Enforces extensions for all import statements
+        // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/extensions.md
+        'import/extensions': [
+          'warn',
+          'ignorePackages',
+          // ... except for the following:
+          {
+            js: 'never',
+            mjs: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+        camelcase: 'off',
+        '@typescript-eslint/naming-convention': [
+          'warn',
+          {
+            selector: 'variable',
+            format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+            leadingUnderscore: 'allow',
+          },
+          {
+            selector: 'parameter',
+            format: ['camelCase'],
+            leadingUnderscore: 'allow',
+          },
+          {
+            selector: 'typeLike',
+            format: ['PascalCase'],
+          },
+        ],
+        '@typescript-eslint/no-unnecessary-condition': 'warn',
+        'no-unused-expressions': 'off',
+        '@typescript-eslint/no-unused-expressions': [
+          'warn',
+          {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: true,
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            ignoreRestSiblings: true,
+            varsIgnorePattern: '^_',
+            argsIgnorePattern: 'req|res|next|^err|^_',
+          },
+        ],
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+          'warn',
+          {
+            functions: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'warn',
+        '@typescript-eslint/prefer-optional-chain': 'warn',
+      },
+    },
+  ],
+};
