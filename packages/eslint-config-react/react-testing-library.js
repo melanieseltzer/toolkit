bd122c86d6e47f0a09ba88288f8df997de259f12
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-03-08 10:28:03 -0800

    Update eslint-plugin-testing-library

--- packages/eslint-config-react/react-testing-library.js
+++ packages/eslint-config-react/react-testing-library.js
@@ -9,6 +9,8 @@ module.exports = {
         'plugin:testing-library/react',
       ],
       rules: {
+        // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-render-in-setup.md
+        'testing-library/no-render-in-setup': 'off',
         // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-explicit-assert.md
         'testing-library/prefer-explicit-assert': 'warn',
         // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-wait-for.md
