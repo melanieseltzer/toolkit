176f09a2579cf316b3fe7a779a7433d5248d0870
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-08-29 16:58:42 -0700

    Update the config to be more explicit
    Explicitly declare all the rules, rather than relying on the defaults set by Prettier (even if they're the same as the defaults).
    
    Clearer because you don't have to go digging through Prettier docs to see what rules are actually set.

--- packages/prettier-config/prettier.config.js
+++ packages/prettier-config/prettier.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   arrowParens: 'avoid',
+  bracketSameLine: false,
   bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
   semi: true,
+  singleAttributePerLine: false,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'none',
   useTabs: false
 };
