2f15d5c66746869063f468b227a808f3af4ba38e
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-09-02 11:45:53 -0700

    Use eslint-plugin-simple-import-sort for sorting

--- packages/eslint-config-base/config/imports.js
+++ packages/eslint-config-base/config/imports.js
@@ -1,6 +1,8 @@
 const jsExtensions = ['.js', '.jsx', '.mjs'];
 
 module.exports = {
+  plugins: ['import', 'simple-import-sort'],
+
   extends: [
     // https://github.com/import-js/eslint-plugin-import/blob/main/config/recommended.js
     'plugin:import/recommended',
@@ -25,6 +27,23 @@ module.exports = {
   },
 
   rules: {
+    // -----------------------
+    // Any overrides to `eslint-plugin-import` recommended
+    // -----------------------
+
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-unresolved.md
+    'import/no-unresolved': [
+      'error',
+      {
+        commonjs: true,
+        caseSensitive: true,
+      },
+    ],
+
+    // -----------------------
+    // Anything else not covered in `eslint-plugin-import` recommended
+    // -----------------------
+
     // Enforces extensions for all import statements
     // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/extensions.md
     'import/extensions': [
@@ -46,25 +65,6 @@ module.exports = {
 
     // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-anonymous-default-export.md
     'import/no-anonymous-default-export': 'warn',
-
-    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md
-    'import/order': [
-      'warn',
-      {
-        'newlines-between': 'always',
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        alphabetize: {
-          order: 'asc',
-        },
-      },
-    ],
 
     // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/newline-after-import.md
     'import/newline-after-import': 'warn',
@@ -102,16 +102,36 @@ module.exports = {
     // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-self-import.md
     'import/no-self-import': 'error',
 
-    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-unresolved.md
-    'import/no-unresolved': [
-      'error',
-      {
-        commonjs: true,
-        caseSensitive: true,
-      },
-    ],
-
     // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-useless-path-segments.md
     'import/no-useless-path-segments': ['warn', { commonjs: true }],
+
+    // -----------------------
+    // Import sorting/ordering
+    // -----------------------
+
+    'simple-import-sort/exports': 'warn',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          // Side effects, i.e. import 'some-package';
+          ['^\\u0000'],
+          // Node.js builtins.
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+          ],
+          // External packages. `react` or 'next' packages come first.
+          ['^react', '^next', '^@?\\w'],
+          // Internal packages that are aliased.
+          ['^(~|@|src|components|app)(/.*|$)'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
   },
 };
