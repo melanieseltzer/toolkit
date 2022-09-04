5b0581615ea39bd22d4aad37b210fec5b7a848d8
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-09-03 23:00:51 -0700

    Flip warns to errors and add some more rules

--- packages/eslint-config-jest/index.js
+++ packages/eslint-config-jest/index.js
@@ -11,24 +11,53 @@ module.exports = {
         // https://github.com/jest-community/eslint-plugin-jest#rules
         'plugin:jest/recommended',
 
+        // https://github.com/jest-community/eslint-plugin-jest#rules
         'plugin:jest/style',
       ],
 
       rules: {
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/no-conditional-in-test.md
+        'jest/no-conditional-in-test': 'error',
+
         // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/no-test-return-statement.md
-        'jest/no-test-return-statement': 'warn',
+        'jest/no-test-return-statement': 'error',
+
+        // -----------------------
+        // Style
+        // -----------------------
+
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/consistent-test-it.md
+        'jest/consistent-test-it': 'error',
+
         // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-comparison-matcher.md
-        'jest/prefer-comparison-matcher': 'warn',
+        'jest/prefer-comparison-matcher': 'error',
+
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-each.md
+        'jest/prefer-each': 'error',
+
         // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-equality-matcher.md
-        'jest/prefer-equality-matcher': 'warn',
+        'jest/prefer-equality-matcher': 'error',
+
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-hooks-in-order.md
+        'jest/prefer-hooks-in-order': 'error',
+
         // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-hooks-on-top.md
-        'jest/prefer-hooks-on-top': 'warn',
+        'jest/prefer-hooks-on-top': 'error',
+
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-mock-promise-shorthand.md
+        'jest/prefer-mock-promise-shorthand': 'error',
+
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-spy-on.md
+        'jest/prefer-spy-on': 'error',
+
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-strict-equal.md
+        'jest/prefer-strict-equal': 'error',
+
         // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/prefer-todo.md
-        'jest/prefer-todo': 'warn',
+        'jest/prefer-todo': 'error',
+
         // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/require-top-level-describe.md
-        'jest/require-top-level-describe': 'warn',
-        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/valid-title.md
-        'jest/valid-title': 'warn',
+        'jest/require-top-level-describe': 'error',
       },
     },
   ],
