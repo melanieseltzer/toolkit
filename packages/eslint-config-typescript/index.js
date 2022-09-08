dc458e81f33088677e306489a02de125c3edcb89
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-09-08 13:35:31 -0700

    Allow TS comments, but only if a justification is provided (#35)
    * Allow TS comments, but only if a justification is provided
    
    * Add comment

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -42,6 +42,17 @@ module.exports = {
         // Any overrides to `@typescript-eslint` recommended
         // -----------------------
 
+        // Allow TS comments, but only if a justification is provided.
+        // https://typescript-eslint.io/rules/ban-ts-comment/
+        '@typescript-eslint/ban-ts-comment': [
+          'error',
+          {
+            'ts-expect-error': 'allow-with-description',
+            'ts-ignore': 'allow-with-description',
+            'ts-nocheck': 'allow-with-description',
+          },
+        ],
+
         // Sometimes necessary when prototyping...
         // https://typescript-eslint.io/rules/no-explicit-any/
         '@typescript-eslint/no-explicit-any': 'off',
